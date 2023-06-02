eddff81c36f3d3c9f8b894fed514dc7a10502e8f
Lrini <iwangzr94@gmail.com>
2023-06-02 19:25:46 +0800

    tambah database

--- db/sdhshop.sql
+++ db/sdhshop.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 02 Jun 2023 pada 13.07
+-- Waktu pembuatan: 02 Jun 2023 pada 13.13
 -- Versi server: 10.4.18-MariaDB
 -- Versi PHP: 7.3.27
 
