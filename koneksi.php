f56a5cae7d11b788ef61fc6b7a13ff9cdb3101a2
Lrini <iwangzr94@gmail.com>
2023-06-04 15:18:53 +0800

    koneksi ke database

---
+++ koneksi.php
@@ -0,0 +1,8 @@
+<?php
+$koneksi = mysqli_connect("localhost","root","","sdhshop"); 
+?>
+
+<!--pak coba sederhanakan codingan arya dan mazmur servernya pakai localhost, username ambil dari aplikasi xampp
+biasanya default namaya root , passwordnya juga kosong kalau default dari xampp, lalu nama database yang kita buat sdhshop-->
+
+
