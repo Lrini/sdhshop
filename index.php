c94ddd9f88cf9c1b6c966eaf396b1aa28a262acf
MazmurBryan <94032400+MazmurBryan@users.noreply.github.com>
2023-06-04 15:03:45 +0800

    mazmur register

--- index.php
+++ index.php
@@ -31,6 +31,7 @@
   <link rel="stylesheet" href="css/mean-menu.css">
   <link rel="stylesheet" href="css/default.css">
   <link rel="stylesheet" href="style.css">
+  <link rel="stylesheet" href="stylelogin.css">
 </head>
 
 <body>
@@ -176,46 +177,47 @@
             <!--tutup modal Login-->
 
             <!-- Modal register -->
-   <div class="modal fade" id="exampleModal1" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-              <div class="modal-dialog" role="document">
-                <div class="modal-content">
-                  <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">Form register</h5>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                      <span aria-hidden="true">&times;</span>
-                    </button>
-                  </div>
-                  <div class="modal-body">
-                      <form action="#">
-                      <div class="form-group">
-                        <label>Nama Anda</label>
-                        <input type="text" class="form-control" id="nama" placeholder="Nama anda ">
-                      </div>
-                      <div class="form-group">
-                        <label>No handphone</label>
-                        <input type="text" class="form-control" id="no_hp" placeholder="No handphone yang dapat dihubungi">
-                      </div>
-                      <div class="form-group">
-                        <label>Alamat</label>
-                        <input type="text" class="form-control" id="alamat" placeholder="alamat anda">
-                      </div>
-                      <div class="form-group">
-                        <label>Email address</label>
-                        <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter email">
-                        <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-                      </div>
-                      <div class="form-group">
-                        <label>Password</label>
-                        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
-                      </div>
-                      <button type="submit" class="btn btn-primary">Submit</button>
-                      </form>
-                  </div>
-                  <div class="modal-footer">
-                  </div>
-                </div>
-              </div>
-            </div>
+            <div class="modal fade" id="exampleModal1" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Form register</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <form action="registrasi.php" method="post">
+          <div class="form-group">
+            <label>Nama Anda</label>
+            <input type="text" class="form-control" name="nama" placeholder="Nama Anda">
+          </div>
+          <div class="form-group">
+            <label>No handphone</label>
+            <input type="text" class="form-control" name="no_hp" placeholder="No handphone yang dapat dihubungi">
+          </div>
+          <div class="form-group">
+            <label>Alamat</label>
+            <input type="text" class="form-control" name="alamat" placeholder="Alamat Anda">
+          </div>
+          <div class="form-group">
+            <label>Email address</label>
+            <input type="email" class="form-control" name="email" placeholder="Enter email">
+            <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+          </div>
+          <div class="form-group">
+            <label>Password</label>
+            <input type="password" class="form-control" name="password" placeholder="Password">
+          </div>
+          <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+      </div>
+      <div class="modal-footer">
+      </div>
+    </div>
+  </div>
+</div>
+
             <!--tutup modal register-->
   <!-- slider area starts -->
   <div class="slider-area slider-2 pt-105">
