82d277e8c932ac8e0dd3f71aab11dfdecf8b9a70
Lrini <iwangzr94@gmail.com>
2023-06-10 11:18:26 +0800

    perbaiki tugas arya

--- submit.php
+++ submit.php
@@ -13,16 +13,9 @@ $email = $_POST['email'];
 $pass = $_POST['pass'];
 
 //buat encrypt password
-$hashedPassword = password_hash($pass, PASSWORD_DEFAULT); 
+$password = hash('md5','$pass');
 
-//insert ke database mysql pada phpmyadmin
-$data = mysqli_query($koneksi,"insert into user (nama,alamat,no_hp,email,pass,level,status) values ('$nama','$alamat','$no_hp','$email','$hashedPassword','user','pasif')");
-//cek kemabli apakah data sudah berhasil masuk ke mysql atau tidak jika tidak akan munculkan alert gagal registrasi
-//kalau berhasil akan pergi kehalaman dalam folder user 
-if($data > 0){
-                    echo'    <script>alert("berhasil");window.location="user/index.php"</script>';     
-//insert ke database mysql pada phpmyadmin
-$data = mysqli_query($koneksi,"insert into user (nama,alamat,no_hp,email,pass,level,status) values ('$nama','$alamat','$no_hp','$email','$pass','user','pasif')");
+$data = mysqli_query($koneksi,"insert into user (nama,alamat,no_hp,email,pass,level,status) values ('$nama','$alamat','$no_hp','$email','$password','user','pasif')");
 //cek kemabli apakah data sudah berhasil masuk ke mysql atau tidak jika tidak akan munculkan alert gagal registrasi
 //kalau berhasil akan pergi kehalaman dalam folder user 
 if($data > 0){
