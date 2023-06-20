bc5b2e31ac6da2dc8faa2dd2c73777c60aa7086a
Lrini <iwangzr94@gmail.com>
2023-06-20 18:53:43 +0800

    fix php

---
+++ admin/public/footer.php
@@ -0,0 +1,28 @@
+
+        <!-- jQuery  -->
+        <script src="../assets/js/jquery.min.js"></script>
+        <script src="../assets/js/bootstrap.bundle.min.js"></script>
+        <script src="../assets/js/waves.js"></script>
+        <script src="../assets/js/simplebar.min.js"></script>
+
+        <!-- Sparkline Js-->
+        <script src="../plugins/jquery-sparkline/jquery.sparkline.min.js"></script>
+
+        <!-- Chart Js-->
+        <script src="../plugins/jquery-knob/jquery.knob.min.js"></script>
+
+        <!-- Chart Custom Js-->
+         <script src="../assets/pages/knob-chart-demo.js"></script>
+
+        <!-- Morris Js-->
+        <script src="../plugins/morris-js/morris.min.js"></script>
+
+        <!-- Raphael Js-->
+        <script src="../plugins/raphael/raphael.min.js"></script>
+
+        <!-- Custom Js -->
+        <script src="../assets/pages/dashboard-demo.js"></script>
+
+        <!-- App js -->
+        <script src="../assets/js/theme.js"></script>
+</html>
