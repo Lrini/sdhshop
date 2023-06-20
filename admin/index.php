b1601691a03bd36e3577f24fd2ef4b0841f144b4
Lrini <iwangzr94@gmail.com>
2023-06-20 19:06:14 +0800

    fix php 1

--- admin/index.php
+++ admin/index.php
@@ -1,14 +1,14 @@
 <!DOCTYPE html>
 <html lang="en">
 
-    
-        
-        <!-- <meta charset="utf-8" />
-        <title>Admin - SDH Shop</title>
+    <head>
+        include "../../header.php";
+        <meta charset="utf-8" />
+        <!-- <title>Admin - SDH Shop</title>
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <meta content="Premium Multipurpose Admin & Dashboard Template" name="description" />
         <meta content="MyraStudio" name="author" />
-        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" /> -->
 
         <!-- App favicon -->
         <!-- <link rel="shortcut icon" href="assets/images/favicon.ico"> -->
@@ -17,7 +17,7 @@
         <!-- <link href="assets/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
         <link href="assets/css/icons.min.css" rel="stylesheet" type="text/css" />
         <link href="assets/css/theme.min.css" rel="stylesheet" type="text/css" /> -->
-
+        </head>
    
 
     <body>
@@ -313,7 +313,7 @@
 
         <!-- App js -->
         <!-- <script src="/assets/js/theme.js"></script> --> 
-        include "../admin/public/footer.php";
+       include "public/footer.php";
 
     </body>
 
--- admin/public/header.php
+++ admin/public/header.php
@@ -1,7 +1,5 @@
-<!DOCTYPE html>
+
 <html>
-<head>
-        <meta charset="utf-8" />
         <title>Admin - SDH Shop</title>
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <meta content="Premium Multipurpose Admin & Dashboard Template" name="description" />
@@ -15,4 +13,4 @@
         <link href="../assets/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
         <link href="../assets/css/icons.min.css" rel="stylesheet" type="text/css" />
         <link href="../assets/css/theme.min.css" rel="stylesheet" type="text/css" />
-</head>
+
