02fdf8cfbd2559dc88ada81763120a83b784efb6
MazmurBryan <94032400+MazmurBryan@users.noreply.github.com>
2023-07-03 18:17:00 +0800

    commit baru

--- admin/kelas.php
+++ admin/kelas.php
@@ -52,26 +52,24 @@ $nama = isset($_SESSION['name']) ? $_SESSION['name'] : '';
     }
 
     function updateKelas($id, $namaKelas, $waliKelas)
-    {
-        global $koneksi;
-        $sql = "UPDATE kelas SET nama_kelas='$namaKelas', walikelas='$waliKelas' WHERE id_kelas='$id'";
-        if (mysqli_query($koneksi, $sql)) {
-            return true;
-        } else {
-            return false;
-        }
+{
+    global $koneksi;
+    $sql = "UPDATE kelas SET nama_kelas='$namaKelas', walikelas='$waliKelas' WHERE id_kelas='$id'";
+    if (mysqli_query($koneksi, $sql)) {
+        return true;
+    } else {
+        return false;
     }
+}
 
-    function deleteKelas($id)
-    {
-        global $koneksi;
-        $sql = "DELETE FROM kelas WHERE id_kelas='$id'";
-        if (mysqli_query($koneksi, $sql)) {
-            return true;
-        } else {
-            return false;
-        }
-    }
+function deleteKelas($kelas_id)
+{
+    global $koneksi;
+    $query = "DELETE FROM kelas WHERE id_kelas = '$kelas_id'";
+    $result = mysqli_query($koneksi, $query);
+    return $result;
+}
+
 
     if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['create'])) {
         $namaKelas = $_POST['nama_kelas'];
@@ -86,31 +84,48 @@ $nama = isset($_SESSION['name']) ? $_SESSION['name'] : '';
         }
     }
 
+
     if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['update'])) {
-        $kelasId = $_POST['kelas_id'];
+        $kelasId = $_POST['id_kelas'];
         $namaKelas = $_POST['nama_kelas'];
         $waliKelas = $_POST['wali_kelas'];
-
-        if (updateKelas($kelasId, $namaKelas, $waliKelas)) {
-            echo "Kelas berhasil diperbarui!<br>";
-            echo "ID Kelas: " . $kelasId . "<br>";
-            echo "Nama Kelas: " . $namaKelas . "<br>";
-            echo "Wali Kelas: " . $waliKelas . "<br><br>";
-        } else {
-            echo "Error: Gagal memperbarui kelas.<br>";
+    
+        try {
+            $pdo = new PDO("mysql:host=localhost;dbname=sdhshop", "root", "");
+            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    
+            // Proses update data kelas
+            $stmt = $pdo->prepare("UPDATE kelas SET nama_kelas = :namaKelas, walikelas = :waliKelas WHERE id_kelas = :kelasId");
+            $stmt->bindParam(':namaKelas', $namaKelas);
+            $stmt->bindParam(':waliKelas', $waliKelas);
+            $stmt->bindParam(':kelasId', $kelasId);
+    
+            if ($stmt->execute()) {
+                echo "Kelas berhasil diperbarui!<br>";
+                echo "ID Kelas: " . $kelasId . "<br>";
+                echo "Nama Kelas: " . $namaKelas . "<br>";
+                echo "Wali Kelas: " . $waliKelas . "<br><br>";
+            } else {
+                echo "Error: Gagal memperbarui kelas.<br>";
+            }
+        } catch (PDOException $e) {
+            echo "Error: " . $e->getMessage();
+            echo "<br>Error Info: ";
+            print_r($stmt->errorInfo());
         }
     }
+    
 
-    if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['delete'])) {
-        $kelasId = $_POST['kelas_id'];
-
-        if (deleteKelas($kelasId)) {
-            echo "Kelas berhasil dihapus!<br>";
-            echo "ID Kelas: " . $kelasId . "<br><br>";
-        } else {
-            echo "Error: Gagal menghapus kelas.<br>";
-        }
+    // Proses hapus kelas
+if (isset($_POST['delete'])) {
+    $kelas_id = $_POST['id_kelas'];
+    $result = deleteKelas($kelas_id);
+    if ($result) {
+        echo "Kelas berhasil dihapus!";
+    } else {
+        echo "Gagal menghapus kelas.";
     }
+}
 
     ?>
 
@@ -138,7 +153,7 @@ $nama = isset($_SESSION['name']) ? $_SESSION['name'] : '';
                     echo "<td>" . $kelas['nama_kelas'] . "</td>";
                     echo "<td>" . $kelas['walikelas'] . "</td>";
                     echo "<td>";
-                    echo "<button type='button' class='btn btn-info btn-sm' data-toggle='modal' data-target='#editKelasModal' data-id='" . $kelas['id_kelas'] . "' data-nama='" . $kelas['nama_kelas'] . "' data-wali='" . $kelas['walikelas'] . "'>Edit</button> ";
+                    echo "<button type='button' class='btn btn-info btn-sm edit-btn' data-toggle='modal' data-target='#editKelasModal' data-id='" . $kelas['id_kelas'] . "' data-nama='" . $kelas['nama_kelas'] . "' data-wali='" . $kelas['walikelas'] . "'>Edit</button> ";
                     echo "<button type='button' class='btn btn-danger btn-sm' data-toggle='modal' data-target='#deleteKelasModal' data-id='" . $kelas['id_kelas'] . "'>Hapus</button>";
                     echo "</td>";
                     echo "</tr>";
@@ -179,64 +194,66 @@ $nama = isset($_SESSION['name']) ? $_SESSION['name'] : '';
     </div>
 
     <!-- Modal Edit Kelas -->
-    <div class="modal fade" id="editKelasModal" tabindex="-1" role="dialog" aria-labelledby="editKelasModalLabel" aria-hidden="true">
-        <div class="modal-dialog" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="editKelasModalLabel">Edit Kelas</h5>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-                <form action="" method="POST">
-                    <div class="modal-body">
-                        <div class="form-group">
-                            <label for="kelas_id">ID Kelas</label>
-                            <input type="text" class="form-control" id="kelas_id" name="kelas_id" readonly>
-                        </div>
-                        <div class="form-group">
-                            <label for="nama_kelas">Nama Kelas</label>
-                            <input type="text" class="form-control" id="nama_kelas" name="nama_kelas" required>
-                        </div>
-                        <div class="form-group">
-                            <label for="wali_kelas">Wali Kelas</label>
-                            <input type="text" class="form-control" id="wali_kelas" name="wali_kelas" required>
-                        </div>
-                    </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Tutup</button>
-                        <button type="submit" class="btn btn-primary" name="update">Simpan Perubahan</button>
-                    </div>
-                </form>
+<div class="modal fade" id="editKelasModal" tabindex="-1" role="dialog" aria-labelledby="editKelasModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="editKelasModalLabel">Edit Kelas</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
             </div>
+            <form action="" method="POST" id="editKelasForm">
+                <div class="modal-body">
+                    <div class="form-group">
+                        <label for="id_kelas">ID Kelas</label>
+                        <input type="text" class="form-control" id="id_kelas" name="id_kelas" readonly>
+                    </div>
+                    <div class="form-group">
+                        <label for="nama_kelas">Nama Kelas</label>
+                        <input type="text" class="form-control" id="nama_kelas" name="nama_kelas" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="wali_kelas">Wali Kelas</label>
+                        <input type="text" class="form-control" id="wali_kelas" name="wali_kelas" required>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="submit" class="btn btn-primary" name="update">Update</button>
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                </div>
+            </form>
         </div>
     </div>
+</div>
 
-    <!-- Modal Hapus Kelas -->
-    <div class="modal fade" id="deleteKelasModal" tabindex="-1" role="dialog" aria-labelledby="deleteKelasModalLabel" aria-hidden="true">
-        <div class="modal-dialog" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="deleteKelasModalLabel">Hapus Kelas</h5>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-                <form action="" method="POST">
-                    <div class="modal-body">
-                        <div class="form-group">
-                            <input type="hidden" class="form-control" id="kelas_id" name="kelas_id" readonly>
-                        </div>
-                        <p>Apakah Anda yakin ingin menghapus kelas ini?</p>
-                    </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Tutup</button>
-                        <button type="submit" class="btn btn-danger" name="delete">Hapus</button>
-                    </div>
-                </form>
+<!-- Modal Delete Kelas -->
+<div class="modal fade" id="deleteKelasModal" tabindex="-1" role="dialog" aria-labelledby="deleteKelasModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="deleteKelasModalLabel">Hapus Kelas</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
             </div>
+            <form action="" method="POST" id="deleteKelasForm">
+                <div class="modal-body">
+                    <p>Apakah Anda yakin ingin menghapus kelas ini?</p>
+                    <div class="form-group">
+                        <label for="id_kelas_delete">ID Kelas</label>
+                        <input type="text" class="form-control" id="id_kelas_delete" name="id_kelas" readonly>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="submit" class="btn btn-danger" name="delete">Hapus</button>
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                </div>
+            </form>
         </div>
     </div>
+</div>
+
 
     <?php include "public/footer.php"; ?>
 
@@ -254,7 +271,7 @@ $nama = isset($_SESSION['name']) ? $_SESSION['name'] : '';
             var wali = button.data('wali');
 
             var modal = $(this);
-            modal.find('#kelas_id').val(id);
+            modal.find('#id_kelas').val(id);
             modal.find('#nama_kelas').val(nama);
             modal.find('#wali_kelas').val(wali);
         });
@@ -264,7 +281,7 @@ $nama = isset($_SESSION['name']) ? $_SESSION['name'] : '';
             var id = button.data('id');
 
             var modal = $(this);
-            modal.find('#kelas_id').val(id);
+            modal.find('#id_kelas').val(id);
         });
     </script>
 </body>
