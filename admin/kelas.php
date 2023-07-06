f425a24a272d369fa473d8cfbc976844536b5594
MazmurBryan <94032400+MazmurBryan@users.noreply.github.com>
2023-07-06 12:14:21 +0800

    kelas

--- admin/kelas.php
+++ admin/kelas.php
@@ -224,6 +224,25 @@ if (!isset($_SESSION['id'])) {
                         </div>
                     </div>
                 </div>
+                  <!-- End Page-content -->
+
+
+                  <footer class="footer">
+                    <div class="container-fluid">
+                        <div class="row">
+                            <div class="col-md-6">
+                                <div class="text-center text-lg-left">
+                                    2020 © Mazmur.arya,shanen.
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="text-right d-none d-lg-block">
+                                    Design & Develop by Lrini
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </footer>
             </div>
 
             <?php include "public/footer.php"; ?>
